af1b41da2947fbc3ba569b9733cf7df96efeb065
Nishant Singh <nishantsingh2n@gmail.com>
2023-08-31 20:34:15 +0530

    JS

---
+++ Lec_31/index.js
@@ -0,0 +1,12 @@
+      console.log("How are you ? Not fine");
+
+      let a=5;
+      console.log(a);
+      let b=("Hello");
+      console.log(b);
+
+      let Name=3;
+      console.log(Name);
+
+      Name="Nishant";
+      console.log(Name);
