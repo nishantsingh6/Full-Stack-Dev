a45fc4fb6c687a4843d598a2cb4e05465cb5ae4a
Nishant Singh <nishantsingh2n@gmail.com>
2023-09-08 14:05:20 +0530

    JS-4 (Loop)

---
+++ Lec_34/index.js
@@ -0,0 +1,40 @@
+console.log(1);
+console.log(2);
+console.log(3);
+
+console.log('loops')
+//for loop
+let n=5;
+for(let i=1; i<=n; i++){
+console.log(i);
+}
+
+//Odd number 1-15
+for(let i=1; i<=15; i=i+2){
+    console.log(i);
+}
+
+//Even Number 2-10
+for(let i=2; i<=10; i=i+2){
+console.log(i);
+}
+
+//Multiply of n number
+for(let i=3; i<=30; i=i+3){
+    console.log(i);
+}
+
+//Nested Loop
+for(let i=1; i<=3; i++){
+    for(let j=1; j<=3; j++){
+        console.log(j);
+    }
+}
+
+
+//While Loop
+let i=1;
+while( i<= 5){
+    console.log(i);
+    i++;
+}
